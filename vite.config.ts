71948ce99c31fb9b3bd2ef9a83e55a92a1e5953e
Seppe Alaerts <seppe.alaerts@craftzing.com>
2025-12-17 13:23:49 +0100

    Publish package

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+
+// Read package.json to get version
+const packageJson = JSON.parse(
+  readFileSync(resolve(process.cwd(), "package.json"), "utf-8")
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()],
-})
+  base: "./",
+  plugins: [
+    react(),
+    {
+      name: "replace-version",
+      transformIndexHtml(html) {
+        return html.replace(/__VERSION__/g, packageJson.version);
+      },
+    },
+  ],
+});
